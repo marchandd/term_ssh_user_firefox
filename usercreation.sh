31e4e84e2b23650f5e491e6c9baa68f8a08d6f5e
marchandd <griffins78@gmail.com>
2015-03-07 18:42:12 +0100

    MarchandD_20150307_v01.01

--- usercreation.sh
+++ usercreation.sh
@@ -11,6 +11,6 @@ DOCKER_ENCRYPYTED_PASSWORD=`perl -e 'print crypt('"$DOCKER_PASSWORD"', "aa"),"\n
 useradd -m -d /home/docker -p $DOCKER_ENCRYPYTED_PASSWORD docker
 sed -Ei 's/adm:x:4:/docker:x:4:docker/' /etc/group
 adduser docker sudo
-RUN echo "docker password access: $DOCKER_PASSWORD" > ~/password.log
+echo "docker password access: $DOCKER_PASSWORD" > ~/password.log
 # Set the default shell as bash for docker user.
 chsh -s /bin/bash docker
